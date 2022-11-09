66cca9dce76ea8eb396fe3b1b8f71902a9db317e
Chris <chrishethertonweb@gmail.com>
2022-11-09 13:44:09 +0100

    task 3

--- task-03/src/App.jsx
+++ task-03/src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return <div className="App">Task 3</div>;
+
+  const [holder, setHolder] = useState()
+
+  useEffect(() => {
+    async function fetchData(){
+      let response = await fetch('https://anapioficeandfire.com/api/characters')
+      const data = await response.json()
+      setHolder(data)
+    }
+    fetchData()
+  }
+    
+  ,[])
+  console.log(holder, 'HERE')
+
+  return (<div className="App">
+    <h1>SONGS OF FIRE AND ICE!</h1>
+    {
+     holder.map((item,idx)=> {
+      return <div key={idx}>
+        <p>link is <a href={item.url}>here</a></p>
+        <p>The name is {item.name}</p>
+        <p>Alias is {item.aliases}</p>
+        <p>The gender is {item.gender}</p>
+
+        </div>
+     })
+    }
+  </div>)
 }
 
 export default App;
