b5b781c4c3cbe6adada92fe9d15c7a8c3c1de9db
Chris <chrishethertonweb@gmail.com>
2022-11-09 12:57:55 +0100

    added all set up for task 2

--- task-02/src/App.jsx
+++ task-02/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./style.css";
+import {useState} from 'react'
 
 function App() {
-  return <div className="App">Task 2</div>;
+
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  function moveHandler() {
+    setX(prev=> prev +10 )
+  }
+  return <div className="App">
+
+    <h1>Move the Box up!</h1>
+    <button onClick={moveHandler}>Move Up</button>
+    <div className="box" ></div>
+  </div>;
 }
 
 export default App;
